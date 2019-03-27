38e41c4fe5511edc8b1f23250ab2e52238ac9546
Cail Borg <Cail.Borg@ofx.com>
2019-03-27 17:20:38 +1100

    no message

--- server.js
+++ server.js
@@ -16,49 +16,46 @@ async function nbaFetch(playerID){
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
         },
     })
-    let commonPlayerInfo = await fetch('https://stats.nba.com/stats/commonplayerinfo?LeagueID=&PlayerID=' + playerID, {
-        mode: 'cors',
-        method: "GET",
-        headers: {
-        "accept-encoding": "Accepflate, sdch",
-        "accept-language": "he-IL,he;q=0.8,en-US;q=0.6,en;q=0.4",
-        "cache-control": "max-age=0",
-        connection: "keep-alive",
-        host: "stats.nba.com",
-        referer: "http://stats.nba.com/",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
-        },
-    })
 
     let dashboardFileStruct = await playerdashboardbygeneralsplits.json()
-    let profileFileStruct = await commonPlayerInfo.json()
     var points = dashboardFileStruct.resultSets[0].rowSet[0][26]
     var rebounds = dashboardFileStruct.resultSets[0].rowSet[0][18]*1.5
     var assists = dashboardFileStruct.resultSets[0].rowSet[0][19]*1.5
     var tov = dashboardFileStruct.resultSets[0].rowSet[0][20]*-2
     var steals = dashboardFileStruct.resultSets[0].rowSet[0][21]*2
     var blocks = dashboardFileStruct.resultSets[0].rowSet[0][22]*2
-    var games = dashboardFileStruct.resultSets[0].rowSet[0][2]
-    var name = profileFileStruct.resultSets[0].rowSet[0][3]
-    // var total = [name, 'TOTAL', points+rebounds+assists+steals+blocks+tov, 'PPG', (points+rebounds+assists+steals+blocks+tov)/games]
     var total = points+rebounds+assists+steals+blocks+tov
-    // console.log(total)
+
     return total
     
 }
 // Looping over the array of players should fill this array with results
-var output = []
+
+const byron = ["201935", "203081", "203497", "202331", "203078", "1627750"];
+const moir = ["203507", "1626157", "202696", "1626156", "203500", "202710"];
+const cail = ["203083", "203999", "203994", "201950", "202699", "202339"];
 
 // // Loop over each of the player IDs and push to our Output array
- async function playerLoop () {
-    const players = ["201935", "203081", "203497", "202331", "203078", "1627750"];
-    await Promise.all(players.map(async (playerID) => {
-      const contents = await nbaFetch(playerID)
+ async function playerLoop(teamID) {   
+    let output = []
+    await Promise.all(teamID.map(async (playerID) => {
+      let contents = await nbaFetch(playerID)
       output.push(contents)
     })).then(function() {
-        console.log(output)
+        // Sort numerically and remove smallest number
+        output.sort(function(a, b){return b-a});
+        output.pop();
+        // Calculate sum of remaining numbers
+        let sum = output.reduce( (a, b) => { return a + b}, 0);
+        // Extract name of variable to use as a label
+        function getVariableName(unknownVariable){
+            return Object.keys(unknownVariable)[0]
+          }
+        console.log(getVariableName({teamID}), sum)
     }, function(err) {
         // error occurred
     });
   }
-  playerLoop()
+  playerLoop(byron)
+  playerLoop(moir)
+  playerLoop(cail)
